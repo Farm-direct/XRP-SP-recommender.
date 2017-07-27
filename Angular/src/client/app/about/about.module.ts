0ce3d0d8cc58bcdb294e0f36f65f727da146b211
AkashRB <akashrrb@gmail,com>
2017-07-27 18:06:27 +0530

    Added the about service

--- Angular/src/client/app/about/about.module.ts
+++ Angular/src/client/app/about/about.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutService } from './shared/about.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule,FormsModule],
   declarations: [AboutComponent],
   exports: [AboutComponent],
   providers: [AboutService]
---
+++ Angular/src/client/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+/**
+ * This class represents the lazy loaded AboutComponent.
+ */
+@Component({
+    moduleId: module.id,
+    selector: 'sd-login',
+    templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css']
+})
+export class LoginComponent {
+}
